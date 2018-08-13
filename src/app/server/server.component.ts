f216ee0572a76400b533b57498105faff7cb3bbb
Thanos <thanos@afrogleap.com>
2018-08-13 11:45:28 +0200

    ngClass directives

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -3,10 +3,11 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-server',
   templateUrl: './server.component.html',
+  styleUrls: ['./server.component.css'],
 })
 
 export class ServerComponent {
-  serverId: number = 10;
+  serverId: number = Math.random();
   serverStatus: string = 'offline';
 
   constructor() {
@@ -16,6 +17,6 @@ export class ServerComponent {
   renderServerStatus = (): string => (this.serverStatus);
 
   getColor = (): string => {
-    return this.serverStatus === 'online' ? 'green' : 'red';
+    return this.serverStatus === 'online' ? 'white' : 'white';
   }
 }
