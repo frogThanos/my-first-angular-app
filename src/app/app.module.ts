ca2498d8b6c9cb353dfa91d65a07fc2381719e16
Thanos <thanos@afrogleap.com>
2018-08-13 09:22:26 +0200

    generated component through cli

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 // Components
 import { AppComponent } from './app.component';
 import { ServerComponent } from './server/server.component';
+import { ServersComponent } from './servers/servers.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ServerComponent,
+    ServersComponent,
   ],
   imports: [
     BrowserModule,
